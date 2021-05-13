8988bebeffffe5fedf0c20ca0ededf62408c8a06
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-05-13 17:45:58 +0200

    I create variable to print winner

--- js/main.js
+++ js/main.js
@@ -44,7 +44,6 @@ const chooseFighter = (fighter) => {
 
         document.getElementById(fighter).onclick = "";
         document.getElementById(fighter).className = "selected";
-
     }
 }
 
@@ -64,26 +63,52 @@ const llenaEquipos = () => {
     `;
 }
 
-
-
-
 const fight = () => {
     let fight = document.getElementById("fight");
     fight.innerHTML =
     `<div class="teamCharacters">
         <div><img class="imgFighters2" src="img/${team1[0].nombre}.png" alt="luchador1"></div>
-
     </div>
-
     <div class="teamCharacters">
         <div><img class="imgFighters2" src="img/${team2[0].nombre}.png" alt="luchador2"></div>
-
     </div>`
     console.log("este team 1 scenariofight", team1);
     console.log("este team 2 scenariofight", team2);
-
 }
+// const fight2 = () => {
 
+//     let fight = document.getElementById("fight");
+//     fight.innerHTML =
+//         console.log('Estoy en fase 5');
+//         let fightWinner = document.getElementById("fightWinner");
+
+//         fightWinner.innerHTML = `<div class="teamCharacters">
+//             <div><img class="imgFighters2" src="img/${team1[1].nombre}.png" alt="luchador1"></div>
+//         </div>
+//         <div class="teamCharacters">
+//             <div><img class="imgFighters2" src="img/${team2[1].nombre}.png" alt="luchador2"></div>
+//         </div>`
+//     console.log("este team 1 scenariofight", team1);
+//     console.log("este team 2 scenariofight", team2);
+
+// }
+ let idWinner  = ""
+
+//  / Show Winner
+// const showWinnerGame = () => {
+//     showWinner = document.getElementById("winner");
+//     showWinnerName = document.getElementById("winnerName");
+//     if(player1.life < 1){
+//         showWinner.innerHTML = `<img id="maquetaEquipos2" src=“img/figthers/${player2.name}.png”>`;
+//         showWinnerName.innerHTML = `${player2.name} wins!`;
+//     }else if(player2.life < 1){
+//         showWinner.innerHTML = `<img id="maquetaEquipos2" src=“img/figthers/${player1.name}.png”>`;
+//         showWinnerName.innerHTML = `${player1.name} wins!`;
+
+//             changeScreen("fase3", "fase4");
+//     };
+//         winner(player1.name)
+// };
 
 
  fighting = () => {
@@ -91,18 +116,35 @@ const fight = () => {
     p1 = team1[0];
     p2 = team2[0];
 
-    console.log("empieza la lucha");
+    // console.log("empieza la lucha");
 
     p1.hit(p2);
     p2.hit(p1);
 
+    healthP1 = document.getElementById("healtP1")
+    healthP2 = document.getElementById("healtP2")
+
     console.log(p1.vida,p2.vida);
-    // do{
-
-    // }while();
-
     if(p1.vida <= 0 || p2.vida <= 0) {
-        console.log('Estoy en fase 5');
+
+        let winner = document.getElementById("winner");
+        winner.innerHTML = `
+        <div class="teamCharacters">
+            <div class="containerWinner" id="winnerName">You Win!</div>
+            <div><img class="winner" src="img/${team2[1].nombre}.png" alt="luchador2"></div>
+        </div>
+        `
         changeFase("fase5");
+        console.log("prueba para imprimir ganador");
     }
+
+ 
+
+
+    //FIGHTER LIFE COUNTING
+
+    // healthP1.value = `${player1.vida}`;
+    // healthP2.value = `${player2.vida}`;
+    // winner();
 }
+
