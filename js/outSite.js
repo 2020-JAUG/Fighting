ffddcc51af43baaecfb82001b88ce6323c7ae10f
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-05-15 13:16:35 +0200

    Test to improve

---
+++ js/outSite.js
@@ -0,0 +1,32 @@
+
+{/* <div class="opponent" >
+<div class="players">PLAYER 2</div>
+<div class="life"><progress id="healthP2" value="400" min="0" max="400"></progress></canvas></div>
+<div class="contrincante" id="opponent2"></div>
+</div>
+*/}
+
+
+//FUNCION PARA CREAR UN SEGUNDO COMBATE CON DOS JUGADORES MAS
+
+// const fight2 = () => {
+
+//     let fight = document.getElementById("fight");
+//     fight.innerHTML =
+//         console.log('Estoy en fase 5');
+//         let fightWinner = document.getElementById("fightWinner");
+
+//         fightWinner.innerHTML = `<div class="teamCharacters">
+//             <div><img class="imgFighters2" src="img/${team1[1].nombre}.png" alt="luchador1"></div>
+//         </div>
+//         <div class="teamCharacters">
+//             <div><img class="imgFighters2" src="img/${team2[1].nombre}.png" alt="luchador2"></div>
+//         </div>`
+//     console.log("este team 1 scenariofight", team1);
+//     console.log("este team 2 scenariofight", team2);
+
+// }
+
+//PARA PONER MAS JUGADORES EN LA FASE SELECCIONADOS
+{/* <div><img class="imgFighters" src="img/${team1[1].nombre}.png" alt="luchador3"></div>
+<div><img class="imgFighters" src="img/${team2[1].nombre}.png" alt="luchador4"></div> */}
